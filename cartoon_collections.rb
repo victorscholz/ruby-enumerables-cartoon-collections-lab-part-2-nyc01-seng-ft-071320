16a50fc7b97453c4a1c0b038da5b8e1be59b4591
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-09 03:37:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? |call|
+  planeteer_calls.any? do |call|
     call.length > 4
   end
 end
