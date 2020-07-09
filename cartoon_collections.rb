8e158f70669eb4502b7adfade78e951326288fab
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-09 03:38:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find? do |call|
-    valid_calls.include?
+    valid_calls.include?(call)
+  end
 end
